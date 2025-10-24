cedd599d34ffa5ed01ae9e96bd0fd3d006deb6cc
asgard2k <taikamng@yahoo.com>
2025-10-23 22:34:05 -0700

    3rd commit

--- lib/main3.dart
+++ lib/main3.dart
@@ -1,28 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: Text('Rows and Columns!'),),
-      body: MyWidget(),
-    ),
-  ));
-}
+void main() => runApp((MaterialApp(home: MyWidget(),)));
 
 class MyWidget extends StatelessWidget {
 
-
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Container(width: 100, height: 100, color: Colors.red),
-        Container(width: 100, height: 100, color: Colors.green),
-        Container(width: 100, height: 100, color: Colors.blue),
-        ],
-      ),
-    );
+  Widget build(BuildContext context) => Scaffold(
+    body: Container(color: Colors.blue),
+  );
   }
-}
